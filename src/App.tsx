b77c3f197cd8201b95d8b38ce658a8e68e67af2e
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-12-23 11:24:49 -0300

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header"
+
 import "./global.css"
 
-function App() {
-  return <h1>Hello World</h1>
+export function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
-
-export default App
